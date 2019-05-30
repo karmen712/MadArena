3ea2f47e7156e6cd0ca0dc4d7566dc4efb417646
KarmenovA <karmenov_am@network.bkv-pvl.kz>
2019-05-30 11:07:27 +0600

    Collision improved, unit out of window border preventing

--- GameModes/sandbox_mode.py
+++ GameModes/sandbox_mode.py
@@ -28,6 +28,7 @@ def sandbox(game):
     random.seed(a='4x4x4x4x')
     sel_rect = pygame.Rect(0, 0, 0, 0)
     sel_pos_start = (0, 0)
+    sea_border_y = game.WIN_HEIGHT * 0.6875
 
     offset = usb.rect.left + 5
     btn_width = w / 16
@@ -79,9 +80,9 @@ def sandbox(game):
                 if e.button == 1:
                     if drag:
                         drag = False
-                        dragged.state = "stand"
                         dragged.move_ip(m_pos)
-                        dragged.target = dragged.rect.midbottom
+                        if sea_border_y > m_pos[1]:
+                            dragged.z = sea_border_y - m_pos[1]
                         dragged = None
                     elif selecting:
                         selecting = False
@@ -94,17 +95,19 @@ def sandbox(game):
                         pass
                         clear_selection()
                 if e.button == 3:
-                    if m_pos[1] > game.WIN_HEIGHT * 0.6875:
+                    if m_pos[1] > sea_border_y:
                         formation_order = 0
+                        frmt_sighn = 1
                         for sel_unit in sel_units:
-                            sel_unit.target = (m_pos[0]+random.randint(1, 7), m_pos[1]+formation_order)
+                            frmt_sighn = frmt_sighn * -1
+                            sel_unit.target = (m_pos[0]+random.randint(1, 7), m_pos[1]+(formation_order*frmt_sighn))
                             formation_order += random.randint(1, 7) + sel_unit.body_height
             if (e.type == pygame.MOUSEBUTTONDOWN) and (e.button == 1):
                 if usb.rect.collidepoint(m_pos):
                     for u_btn in u_btns:
                         if u_btn.fillrect.collidepoint(m_pos):
                             drag = True
-                            dragged = Human(m_pos, 100, "drag", u_btn.data)
+                            dragged = Human.Human(m_pos, 100, "drag", u_btn.data)
                             dragged.id = len(units)
                             units.append(dragged)
                 elif not selecting:
@@ -115,21 +118,40 @@ def sandbox(game):
             sort_units()
         for unit in units:
             unit.draw(screen)
+            collision_speed_x = unit.move_speed_x + 2
+            collision_speed_y = unit.move_speed_y + 2
+            rand = random.randint(3, 7)
+            if unit.half_rect.center[1] < sea_border_y and unit.state != "falling" and unit.state != "drag":
+                unit.rect.y += collision_speed_x
+                unit.target = (unit.target[0], sea_border_y + rand)
+            if unit.half_rect.center[1] > game.WIN_HEIGHT:
+                unit.rect.y -= collision_speed_x
+                unit.target = (unit.target[0], game.WIN_HEIGHT - rand)
+            if unit.half_rect.center[0] > game.WIN_WIDTH:
+                unit.rect.x -= collision_speed_x
+                unit.target = (game.WIN_WIDTH - rand, unit.target[1])
+            if unit.half_rect.center[0] < 1:
+                unit.rect.x += collision_speed_x
+                unit.target = (rand, unit.target[1])
             screen.blit(game.font.render(unit.state, False, (155, 55, 55)), (unit.rect.topleft[0], unit.rect.topleft[1]-10))
             for unit2 in units:
                 if unit.id == unit2.id:
                     continue
                 if unit.half_rect.colliderect(unit2.half_rect):
                     if unit.half_rect.center[0] > unit2.half_rect.center[0]:
-                        unit.rect.x += 1
-                        unit2.rect.x -= 1
+                        unit.rect.x += collision_speed_x
+                        unit2.rect.x -= collision_speed_x
+                        unit.target = (unit.target[0] + collision_speed_x, unit.target[1])
                     else:
-                        unit.rect.x -= 1
-                        unit2.rect.x += 1
+                        unit.rect.x -= collision_speed_x
+                        unit2.rect.x += collision_speed_x
+                        unit.target = (unit.target[0] - collision_speed_x, unit.target[1])
                     if unit.half_rect.center[1] > unit2.half_rect.center[1]:
-                        unit.rect.y += 1
-                        unit2.rect.y -= 1
+                        unit.rect.y += collision_speed_y
+                        unit2.rect.y -= collision_speed_y
+                        unit.target = (unit.target[0], unit.target[1] + collision_speed_y )
                     else:
-                        unit.rect.y -= 1
-                        unit2.rect.y += 1
+                        unit.rect.y -= collision_speed_y
+                        unit2.rect.y += collision_speed_y
+                        unit.target = (unit.target[0], unit.target[1] - collision_speed_y )
         pygame.display.update()
