5fd7d603a78af3c2262df91a1b7b0a6865e374ca
KarmenovA <karmenov_am@network.bkv-pvl.kz>
2019-05-31 11:42:04 +0600

    improving attack, still in process

--- GameModes/sandbox_mode.py
+++ GameModes/sandbox_mode.py
@@ -6,6 +6,7 @@ from GameHUD.unit_select_bar import UnitSelectBar
 from Interface.Button import Button
 from Units.Human import Human
 import random
+import math
 
 
 def sandbox(game):
@@ -55,16 +56,31 @@ def sandbox(game):
             s_unit.selected = False
         sel_units.clear()
 
+    def get_unit_by_id(uid):
+        for un in units:
+            if uid == un.id:
+                return un
+
+    def unit_able_to(u, action):
+        if action == "attack":
+            return u.state in ["stand", "attackmove"] and u.attack_target is None
+        if action == "be_attacked":
+            return u.state in ["stand", "moving", "falling", "stunned"]
+
+    def get_distance(pos1, pos2):
+        return math.hypot(pos1[0] - pos2[0], pos1[1] - pos2[1])
+
     while 1:  # Основной цикл программы
+        if game.state != "Sandbox":
+            break
         timer.tick(100)
         second += 100
         m_pos = pygame.mouse.get_pos()
-        if game.state != "Sandbox":
-            break
-        screen.blit(bg.image, bg.rect)  # Каждую итерацию необходимо всё перерисовывать
 
+        screen.blit(bg.image, bg.rect)  # Каждую итерацию необходимо всё перерисовывать
         usb.draw(screen)
         draw_buttons()
+
         if selecting:
             sel_rect.topleft = sel_pos_start
             sel_rect.width = m_pos[0] - sel_pos_start[0]
@@ -74,7 +90,8 @@ def sandbox(game):
             dragged.move_ip(m_pos)
             if sea_border_y > m_pos[1]:
                 dragged.z = sea_border_y - m_pos[1]
-        for e in pygame.event.get():  # Обрабатываем события
+        # region EVENT DETECTION ---------------------------------------------------------------------------------
+        for e in pygame.event.get():
             if e.type == pygame.QUIT:
                 raise SystemExit
             if e.type == pygame.KEYDOWN and e.key == pygame.K_ESCAPE:
@@ -118,6 +135,7 @@ def sandbox(game):
                 elif not selecting:
                     selecting = True
                     sel_pos_start = m_pos
+        # endregion
         if second == 1000:
             second = 0
             sort_units()
@@ -126,6 +144,7 @@ def sandbox(game):
             collision_speed_x = unit.move_speed_x + 2
             collision_speed_y = unit.move_speed_y + 2
             rand = random.randint(3, 7)
+            # region BORDER INTERSECT ----------------------------------------------------------------------------
             if unit.half_rect.center[1] < sea_border_y and unit.state != "falling" and unit.state != "drag":
                 unit.rect.y += collision_speed_y
                 unit.target = (unit.target[0], sea_border_y + rand)
@@ -138,7 +157,26 @@ def sandbox(game):
             if unit.half_rect.center[0] < 1:
                 unit.rect.x += collision_speed_x
                 unit.target = (rand, unit.target[1])
-            screen.blit(game.font.render(unit.state, False, (155, 55, 55)), (unit.rect.topleft[0], unit.rect.topleft[1]-10))
+            # endregion
+
+            attack_targets = []
+            if unit_able_to(unit, "attack"):
+                for unit2 in units:
+                    if unit.id == unit2.id:
+                        continue
+                    if unit_able_to(unit2, "be_attacked") and get_distance(unit.rect.midbottom, unit2.rect.midbottom) < unit.enemy_detect_range:
+                        attack_targets.append(unit2)
+
+            if len(attack_targets) > 1:
+                min_dist = unit.enemy_detect_range
+                for tr in attack_targets:
+                    dist = get_distance(unit.rect.midbottom, tr.rect.midbottom)
+                    if dist < min_dist:
+                        min_dist = dist
+                        unit.attack_target = tr
+            elif len(attack_targets) == 1:
+                unit.attack_target = attack_targets[0]
+
             for unit2 in units:
                 if unit.id == unit2.id:
                     continue
@@ -154,9 +192,24 @@ def sandbox(game):
                     if unit.half_rect.center[1] > unit2.half_rect.center[1]:
                         unit.rect.y += collision_speed_y
                         unit2.rect.y -= collision_speed_y
-                        unit.target = (unit.target[0], unit.target[1] + collision_speed_y )
+                        unit.target = (unit.target[0], unit.target[1] + collision_speed_y)
                     else:
                         unit.rect.y -= collision_speed_y
                         unit2.rect.y += collision_speed_y
-                        unit.target = (unit.target[0], unit.target[1] - collision_speed_y )
+                        unit.target = (unit.target[0], unit.target[1] - collision_speed_y)
+
+            # if unit.team != unit2.team and unit_able_to(unit2, "be_attacked") and unit_able_to(unit, "attack"):
+            #     if unit.attack_rect.colliderect(unit2.rect):
+            #         a_targets[unit2.id] = math.hypot(unit.rect.x - unit2.rect.x, unit.rect.y - unit2.rect.y)
+            #     elif not unit.attack_rect.colliderect(unit2.rect) and unit2.id in a_targets:
+            #         del a_targets[unit2.id]
+            # if len(a_targets) > 0:
+            #     a_trgt = get_unit_by_id(min(a_targets, key=a_targets.get))
+            #     if unit.state == "stand":
+            #         close_to_attack_target(unit, a_trgt)
+            #         unit.attack_target = a_trgt
+            # else:
+            #     unit.attack_target = None
+
+            screen.blit(game.font.render(str(unit.state), False, (155, 55, 55)), (unit.rect.topleft[0], unit.rect.topleft[1] - 10))
         pygame.display.update()
--- Units/Human/Human.py
+++ Units/Human/Human.py
@@ -1,8 +1,8 @@
 from pygame import *
-import pyganim
+import math
 from System.resoursepath import resource_path
 import random
-from Menus.options import team_colors
+import Menus.options as options
 from Units.Human.human_animations import HumanAnimations
 
 
@@ -11,10 +11,15 @@ class Human:
         self.image = image.load(resource_path("Media/Sprites/Units/Human/human.png"))
         self.rect = self.image.get_rect(center=pos)
 
-        self.Animations = HumanAnimations(100)
-        self.Attack_cooldown = 1500
+        self.animations = HumanAnimations(100)
+        self.attack_cd = 1500
+        self.attack_cur_cd = 1500
+        self.attack_damage = 10
+        self.attack_range = 40
+        self.attack_target = None
         self.body_height = self.rect.width * 0.23
         self.dir = random.randint(0, 1)  # 0 - LEFT 1- RIGHT
+        self.enemy_detect_range = 150
         self.half_rect = self.rect.copy()
         self.half_rect.height = self.body_height
         self.half_rect.center = self.rect.midbottom
@@ -31,13 +36,29 @@ class Human:
         self.team = team
         self.xvel = 0
         self.xspeed = 0
-        self.gravity = 0.98
+
+        self.yvel = 0
         self.yspeed = 0
         self.z = 0
 
+    @staticmethod
+    def get_distance(p1, p2):
+        return math.hypot(p1[0] - p2[0], p1[1] - p2[1])
+
+    def get_dist_to_trgt(self):
+        return self.get_distance(self.rect.midbottom, self.attack_target.rect.midbottom)
+
+    def end_of_attacking(self):
+        if self.attack_target is None:
+            return True
+        return ((self.attack_target.hp < 1) or (self.get_dist_to_trgt() > self.attack_range)
+                or (self.state != "attack") or (self.attack_target.state == "dead"))
+
+    # def end_of_fight something that I forget
+
     def draw(self, screen):
         def draw_hp_bar():
-            draw.rect(screen, team_colors[self.team], Rect(self.rect.x + 2, self.rect.y - 12, (self.max_hp / 4) + 2, 3), 1)  # контур полоски hp
+            draw.rect(screen, options.team_colors[self.team], Rect(self.rect.x + 2, self.rect.y - 12, (self.max_hp / 4) + 2, 3), 1)  # контур полоски hp
             draw.rect(screen, (15, 55, 15), Rect(self.rect.x + 3, self.rect.y - 11, self.hp / 4, 1), 0)             # текущее количество hp
 
         def stop_moving():
@@ -71,13 +92,70 @@ class Human:
                 else:
                     self.yspeed = self.move_speed_y * -1
 
+        def close_to_attack_target():
+            x1, y1 = self.half_rect.center
+            x2, y2 = self.attack_target.half_rect.center
+            tx, ty = self.attack_target.half_rect.center
+            if x1 > x2:
+                tx += self.attack_range / 2.5
+            else:
+                tx -= self.attack_range / 2.5
+            self.target = (tx, ty)
+
+        def die():
+            self.yvel = 2
+            self.xvel = 2
+            self.state = "dead"
+            self.target = self.rect.midbottom
+            self.attack_target = None
+            if self.dir == 0:
+                self.image = image.load(resource_path("Media/Sprites/Units/Human/human_dead_left.png"))
+            else:
+                self.image = image.load(resource_path("Media/Sprites/Units/Human/human_dead_right.png"))
+
         self.half_rect.center = self.rect.midbottom
         self.shadow_rect.x, self.shadow_rect.y = self.half_rect.x, (self.half_rect.y + self.z)
+
+        # region SPEED, VELOCITY and Z regulations -------------------------------------------------------------
+        if self.xvel > 0:
+            self.xspeed += self.xvel
+            self.xvel -= options.friction
+            self.xspeed -= options.friction
+        elif self.xvel < 0:
+            self.xvel = 0
+
+        if self.yvel > 0:
+            self.yspeed += self.yvel
+            self.yvel -= options.gravity
+        elif self.yvel < 0:
+            self.yvel = 0
+
+        if self.xspeed != 0:
+            if self.state != "moving":
+                if abs(self.xspeed) < options.friction:
+                    self.xspeed = 0
+                else:
+                    if self.xspeed > options.friction:
+                        self.xspeed -= options.friction
+                    else:
+                        self.xspeed += options.friction
+            self.rect.move_ip(self.xspeed, 0)
+
+        if self.yspeed != 0:
+            if self.state not in ("moving", "falling") and self.z > 0:
+                if self.yspeed < options.gravity:
+                    self.yspeed = 0
+                else:
+                    self.yspeed -= options.gravity
+            self.rect.move_ip(0, self.yspeed)
+            if self.yvel > 0:
+                self.z += self.yspeed
 
         if self.z > 0:
             if self.state != "drag":
                 self.state = "falling"
-                self.yspeed += self.gravity
+                self.yspeed += options.gravity
+        # endregion
 
         if self.hp > 0:
             draw_hp_bar()
@@ -85,30 +163,35 @@ class Human:
                 self.state = "moving"
                 walk_to_target()
             if self.selected:
-                draw.ellipse(screen, team_colors[self.team], self.half_rect, 1)
+                draw.ellipse(screen, options.team_colors[self.team], self.half_rect, 1)
+                draw.circle(screen, options.team_colors[self.team], self.rect.midbottom, self.attack_range, 2)
+                draw.circle(screen, options.team_colors[self.team], self.rect.midbottom, self.enemy_detect_range, 1)
         else:
-            self.state = "dead"
-            self.image = image.load(resource_path("Media/Sprites/Units/Human/human_dead.png"))
+            die()
 
         if self.state == "drag":
-            self.Animations.Drag.blit(screen, self.rect.midtop)
+            self.animations.Drag.blit(screen, self.rect.midtop)
 
         elif self.state == "moving":
-            self.rect.move_ip(self.xspeed, self.yspeed)
-            draw.line(screen, team_colors[self.team], self.target, self.rect.center, 1)
+            draw.line(screen, options.team_colors[self.team], self.target, self.rect.center, 1)
             if self.dir == 0:
-                self.Animations.Walk_left.blit(screen, (self.rect.x, self.rect.y))
+                self.animations.Walk_left.blit(screen, (self.rect.x, self.rect.y))
             else:
-                self.Animations.Walk_right.blit(screen, (self.rect.x, self.rect.y))
+                self.animations.Walk_right.blit(screen, (self.rect.x, self.rect.y))
 
         elif self.state == "stand":
+            if self.attack_target is not None:
+                if self.get_dist_to_trgt() <= self.attack_range:
+                    self.state = "attack"
+                    return
+                else:
+                    close_to_attack_target()
             if self.dir == 0:
-                self.Animations.Stand_left.blit(screen, (self.rect.x, self.rect.y))
+                self.animations.Stand_left.blit(screen, (self.rect.x, self.rect.y))
             else:
-                self.Animations.Stand_right.blit(screen, (self.rect.x, self.rect.y))
+                self.animations.Stand_right.blit(screen, (self.rect.x, self.rect.y))
 
         elif self.state == "falling":
-            self.rect.move_ip(self.xspeed, self.yspeed)
             self.z -= self.yspeed
             if self.dir == 0:
                 self.image = image.load(resource_path("Media/Sprites/Units/Human/human_falling_left.png"))
@@ -122,6 +205,23 @@ class Human:
 
         elif self.state == "dead":
             screen.blit(self.image, self.rect)
+
+        elif self.state == "attack":
+            if self.attack_cur_cd <= 0:
+                self.attack_cur_cd = self.attack_cd
+                if self.get_dist_to_trgt() <= self.attack_range and self.attack_target is not None:
+                    self.attack_target.hp -= self.attack_damage
+                if self.end_of_attacking():
+
+                    self.attack_target = None
+                    self.state = "stand"
+                    return
+            else:
+                self.attack_cur_cd -= 100
+            if self.rect.midbottom[0] > self.attack_target.rect.midbottom[0]:
+                self.animations.Attack_left.blit(screen, (self.rect.x, self.rect.y))
+            else:
+                self.animations.Attack_right.blit(screen, (self.rect.x, self.rect.y))
 
     def move_ip(self, pos):
         self.rect.topright = pos
