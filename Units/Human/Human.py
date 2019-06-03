f293a6d82cf12c19764e18c89434089ef202ed88
KarmenovA <karmenov_am@network.bkv-pvl.kz>
2019-06-03 08:05:06 +0600

    prev

--- Units/Human/Human.py
+++ Units/Human/Human.py
@@ -12,11 +12,13 @@ class Human:
         self.rect = self.image.get_rect(center=pos)
 
         self.animations = HumanAnimations(100)
+        # region ATTACK
         self.attack_cd = 1500
         self.attack_cur_cd = 1500
         self.attack_damage = 10
         self.attack_range = 40
         self.attack_target = None
+        # endregion
         self.body_height = self.rect.width * 0.23
         self.dir = random.randint(0, 1)  # 0 - LEFT 1- RIGHT
         self.enemy_detect_range = 150
@@ -25,6 +27,7 @@ class Human:
         self.half_rect.center = self.rect.midbottom
         self.hp = max_hp
         self.id = None
+        self.killer = None
         self.max_hp = max_hp
         self.moving = False
         self.move_speed_x = 2.0
@@ -36,30 +39,38 @@ class Human:
         self.team = team
         self.xvel = 0
         self.xspeed = 0
-
         self.yvel = 0
         self.yspeed = 0
         self.z = 0
+        # !!!!!!!!!!!!!!!!--------------------------------
+        self.font = font.SysFont('Arial', 14)
 
     @staticmethod
     def get_distance(p1, p2):
         return math.hypot(p1[0] - p2[0], p1[1] - p2[1])
 
-    def get_dist_to_trgt(self):
-        return self.get_distance(self.rect.midbottom, self.attack_target.rect.midbottom)
+    def get_dist_to_attack_trgt(self):
+        if self.attack_target is not None:
+            return self.get_distance(self.rect.midbottom, self.attack_target.rect.midbottom)
+        else:
+            return 0
 
     def end_of_attacking(self):
         if self.attack_target is None:
             return True
-        return ((self.attack_target.hp < 1) or (self.get_dist_to_trgt() > self.attack_range)
+        return ((self.attack_target.hp < 1) or (self.get_dist_to_attack_trgt() > self.attack_range)
                 or (self.state != "attack") or (self.attack_target.state == "dead"))
 
-    # def end_of_fight something that I forget
+    def end_of_fight(self):
+        if self.attack_target is None:
+            return True
+        return ((self.attack_target.hp < 1) or (self.get_dist_to_attack_trgt() > self.enemy_detect_range)
+                or (self.state != "attack") or (self.attack_target.state == "dead"))
 
     def draw(self, screen):
         def draw_hp_bar():
             draw.rect(screen, options.team_colors[self.team], Rect(self.rect.x + 2, self.rect.y - 12, (self.max_hp / 4) + 2, 3), 1)  # контур полоски hp
-            draw.rect(screen, (15, 55, 15), Rect(self.rect.x + 3, self.rect.y - 11, self.hp / 4, 1), 0)             # текущее количество hp
+            draw.rect(screen, options.team_colors[self.team], Rect(self.rect.x + 3, self.rect.y - 11, self.hp / 4, 1), 0)             # текущее количество hp
 
         def stop_moving():
             self.xspeed = 0
@@ -93,18 +104,25 @@ class Human:
                     self.yspeed = self.move_speed_y * -1
 
         def close_to_attack_target():
-            x1, y1 = self.half_rect.center
-            x2, y2 = self.attack_target.half_rect.center
-            tx, ty = self.attack_target.half_rect.center
+            x1, y1 = self.rect.midbottom
+            x2, y2 = self.attack_target.rect.midbottom
+            tx, ty = self.attack_target.rect.midbottom
+            distance_to_move = 0
+            if self.get_dist_to_attack_trgt() > self.attack_range:
+                distance_to_move = self.get_dist_to_attack_trgt() - self.attack_range
+
             if x1 > x2:
-                tx += self.attack_range / 2.5
+                tx += distance_to_move
             else:
-                tx -= self.attack_range / 2.5
+                tx -= distance_to_move
             self.target = (tx, ty)
 
         def die():
-            self.yvel = 2
-            self.xvel = 2
+            self.yvel = -40
+            if self.killer.rect.center[0] > self.rect.center[0]:
+                self.xvel = 10
+            else:
+                self.xvel = -10
             self.state = "dead"
             self.target = self.rect.midbottom
             self.attack_target = None
@@ -113,6 +131,17 @@ class Human:
             else:
                 self.image = image.load(resource_path("Media/Sprites/Units/Human/human_dead_right.png"))
 
+        def land_hit():
+            self.attack_cur_cd = self.attack_cd
+            if self.get_dist_to_attack_trgt() <= self.attack_range and self.attack_target is not None:
+                self.attack_target.hp -= self.attack_damage
+                if self.attack_target.hp < 1:
+                    self.attack_target.killer = self
+            if self.end_of_attacking():
+                if self.end_of_fight():
+                    self.attack_target = None
+                self.state = "stand"
+
         self.half_rect.center = self.rect.midbottom
         self.shadow_rect.x, self.shadow_rect.y = self.half_rect.x, (self.half_rect.y + self.z)
 
@@ -120,53 +149,52 @@ class Human:
         if self.xvel > 0:
             self.xspeed += self.xvel
             self.xvel -= options.friction
-            self.xspeed -= options.friction
         elif self.xvel < 0:
-            self.xvel = 0
+            self.xspeed += self.xvel
+            self.xvel += options.friction
 
         if self.yvel > 0:
             self.yspeed += self.yvel
             self.yvel -= options.gravity
         elif self.yvel < 0:
-            self.yvel = 0
+            self.yspeed += self.yvel
+            self.yvel += options.gravity
 
         if self.xspeed != 0:
             if self.state != "moving":
-                if abs(self.xspeed) < options.friction:
+                if abs(self.xspeed) <= options.friction:
                     self.xspeed = 0
                 else:
                     if self.xspeed > options.friction:
                         self.xspeed -= options.friction
-                    else:
+                    elif self.xspeed < options.friction:
                         self.xspeed += options.friction
             self.rect.move_ip(self.xspeed, 0)
 
         if self.yspeed != 0:
-            if self.state not in ("moving", "falling") and self.z > 0:
-                if self.yspeed < options.gravity:
-                    self.yspeed = 0
-                else:
-                    self.yspeed -= options.gravity
             self.rect.move_ip(0, self.yspeed)
-            if self.yvel > 0:
-                self.z += self.yspeed
+            if self.z <= 0 and self.state != "moving":
+                self.yspeed = 0
+                self.yvel = 0
+                self.target = self.rect.midbottom
+            if self.yspeed > 0:
+                self.z -= self.yspeed
 
         if self.z > 0:
             if self.state != "drag":
                 self.state = "falling"
-                self.yspeed += options.gravity
+            self.yspeed += options.gravity
         # endregion
 
         if self.hp > 0:
             draw_hp_bar()
             if not self.half_rect.collidepoint(self.target) and (self.state != "drag") and (self.state != "falling"):
                 self.state = "moving"
-                walk_to_target()
             if self.selected:
                 draw.ellipse(screen, options.team_colors[self.team], self.half_rect, 1)
                 draw.circle(screen, options.team_colors[self.team], self.rect.midbottom, self.attack_range, 2)
                 draw.circle(screen, options.team_colors[self.team], self.rect.midbottom, self.enemy_detect_range, 1)
-        else:
+        elif self.state != "dead":
             die()
 
         if self.state == "drag":
@@ -174,6 +202,12 @@ class Human:
 
         elif self.state == "moving":
             draw.line(screen, options.team_colors[self.team], self.target, self.rect.center, 1)
+            walk_to_target()
+            if self.attack_target is not None:
+                if self.get_dist_to_attack_trgt() <= self.attack_range:
+                    self.state = "attack"
+                else:
+                    close_to_attack_target()
             if self.dir == 0:
                 self.animations.Walk_left.blit(screen, (self.rect.x, self.rect.y))
             else:
@@ -181,9 +215,8 @@ class Human:
 
         elif self.state == "stand":
             if self.attack_target is not None:
-                if self.get_dist_to_trgt() <= self.attack_range:
+                if self.get_dist_to_attack_trgt() <= self.attack_range:
                     self.state = "attack"
-                    return
                 else:
                     close_to_attack_target()
             if self.dir == 0:
@@ -208,20 +241,16 @@ class Human:
 
         elif self.state == "attack":
             if self.attack_cur_cd <= 0:
-                self.attack_cur_cd = self.attack_cd
-                if self.get_dist_to_trgt() <= self.attack_range and self.attack_target is not None:
-                    self.attack_target.hp -= self.attack_damage
-                if self.end_of_attacking():
-
-                    self.attack_target = None
-                    self.state = "stand"
-                    return
+                land_hit()
             else:
                 self.attack_cur_cd -= 100
-            if self.rect.midbottom[0] > self.attack_target.rect.midbottom[0]:
+            if self.rect.midbottom[0] > self.target[0]:
                 self.animations.Attack_left.blit(screen, (self.rect.x, self.rect.y))
             else:
                 self.animations.Attack_right.blit(screen, (self.rect.x, self.rect.y))
+
+        elif self.state == "attack_move":
+            pass
 
     def move_ip(self, pos):
         self.rect.topright = pos
