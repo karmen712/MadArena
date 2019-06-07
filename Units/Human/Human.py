23cd8df539aa1d760343d15a889460af68c8cebe
KarmenovA <karmenov_am@network.bkv-pvl.kz>
2019-06-07 13:37:44 +0600

    find_point_to_attack function improved

--- Units/Human/Human.py
+++ Units/Human/Human.py
@@ -12,13 +12,13 @@ class Human:
         self.rect = self.image.get_rect(center=pos)
 
         # region SELF VARIABLES ----------------------------------------------------------
-        self.animations = HumanAnimations(200)
+        self.animations = HumanAnimations(100)
         # region ATTACK
         self.attack_damage = 10
         self.attack_range = 45
         self.attack_target = None
         # endregion
-        self.body_height = self.rect.width * 0.23
+        self.body_height = self.rect.height * 0.23
         self.dir = random.randint(0, 1)  # 0 - LEFT 1- RIGHT
         self.enemy_detect_range = 150
         self.half_rect = self.rect.copy()
@@ -61,21 +61,33 @@ class Human:
         self.animations.Attack_right.stop()
         self.state = "stand"
 
-    def close_to_attack_target(self):
+    def find_point_to_attack(self):
         if self.end_fight():
             self.stop_attacking()
             return
-        x1, y1 = self.rect.midbottom
-        x2, y2 = self.attack_target.rect.midbottom
-        tx, ty = self.attack_target.rect.midbottom
-        distance_to_move = self.attack_range / 2.5
+
         if not self.end_attacking() and self.able_to_attack():
             self.attack_start()
             return
-        if x1 > x2:
-            tx += distance_to_move
-        else:
-            tx -= distance_to_move
+
+        x1, y1 = self.rect.midbottom
+        x2, y2 = self.attack_target.rect.midbottom
+
+        distance_to_move = (self.get_dist_to_attack_trgt() - self.attack_range) + (self.attack_target.rect.width/2)
+        offset_mul = (distance_to_move / self.get_dist_to_attack_trgt())
+
+        tx = x1 + offset_mul*(x2-x1)
+        ty = y1 + offset_mul*(y2-y1)
+        if not self.in_target_y_width() and self.get_dist_to_attack_trgt() <= self.attack_range:
+            if x1 - self.rect.width < x2 < x1 + self.rect.width:
+                if x1 < x2:
+                    tx += self.rect.width/2
+                else:
+                    tx -= self.rect.width/2
+            if y1 < y2:
+                ty += self.attack_target.body_height
+            else:
+                ty -= self.attack_target.body_height
         self.target = (tx, ty)
 
     def die(self):
@@ -96,32 +108,6 @@ class Human:
         def draw_hp_bar():
             draw.rect(screen, options.team_colors[self.team], Rect(self.rect.x + 2, self.rect.y - 12, (self.max_hp / 4) + 2, 3), 1)  # контур полоски hp
             draw.rect(screen, (70, 200, 70), Rect(self.rect.x + 3, self.rect.y - 11, self.hp / 4, 1), 0)             # текущее количество hp
-
-        def walk_to_target():
-            bx, by = self.rect.midbottom
-            tx, ty = self.target
-            x_condition = bx - (self.half_rect.width*0.6) <= tx <= bx + (self.half_rect.width*0.6)
-            y_condition = by - (self.body_height*0.6) <= ty <= by + (self.body_height*0.6)
-            if x_condition and y_condition:
-                self.stop_moving()
-                self.state = "stand"
-                return
-            if x_condition:
-                self.xspeed = 0
-            if y_condition:
-                self.yspeed = 0
-            if not x_condition:
-                if tx > bx:
-                    self.dir = 1
-                    self.xspeed = self.move_speed_x
-                else:
-                    self.dir = 0
-                    self.xspeed = self.move_speed_x * -1
-            if not y_condition:
-                if ty > by:
-                    self.yspeed = self.move_speed_y
-                else:
-                    self.yspeed = self.move_speed_y * -1
 
         def land_hit():
             # draw.circle(screen, (0, 0, 255), self.rect.center, 15, 0)
@@ -155,9 +141,7 @@ class Human:
 
         elif self.state == "moving":
             draw.line(screen, options.team_colors[self.team], self.target, self.rect.center, 1)
-            walk_to_target()
-            if self.attack_target is not None:
-                self.close_to_attack_target()
+            self.walk_to_target()
             if self.dir == 0:
                 self.animations.Walk_left.blit(screen, (self.rect.x, self.rect.y))
             else:
@@ -166,7 +150,7 @@ class Human:
         elif self.state == "stand":
             self.stop_moving()
             if self.attack_target is not None:
-                    self.close_to_attack_target()
+                    self.find_point_to_attack()
             if self.dir == 0:
                 self.animations.Stand_left.blit(screen, (self.rect.x, self.rect.y))
             else:
@@ -254,17 +238,38 @@ class Human:
         self.state = "stand"
 
     def stop_moving(self):
-        self.xspeed = 0
-        self.yspeed = 0
+        self.xspeed = 0.0
+        self.yspeed = 0.0
         self.z = 0.0
 
         self.target = self.rect.midbottom
 
-# if self.attack_stand_time > 0:
-#     self.attack_stand_time - options.milliseconds
-#     if self.dir == 0:
-#         self.image = image.load(resource_path("Media/Sprites/Units/Human/Attack/Left/human_attack_left3.png"))
-#     else:
-#         self.image = image.load(resource_path("Media/Sprites/Units/Human/Attack/Right/human_attack_right3.png"))
-#     screen.blit(self.image, self.rect)
-#     return
+    def walk_to_target(self):
+        bx, by = self.rect.midbottom
+        tx, ty = self.target
+        x_condition = bx - (self.half_rect.width/4) <= tx <= bx + (self.half_rect.width/4)
+        y_condition = by - (self.body_height/4) <= ty <= by + (self.body_height/4)
+
+        if not self.end_fight():
+            self.find_point_to_attack()
+
+        if x_condition and y_condition:
+            self.stop_moving()
+            self.state = "stand"
+            return
+        if x_condition:
+            self.xspeed = 0.0
+        if y_condition:
+            self.yspeed = 0.0
+        if not x_condition:
+            if tx > bx:
+                self.dir = 1
+                self.xspeed = self.move_speed_x
+            else:
+                self.dir = 0
+                self.xspeed = self.move_speed_x * -1
+        if not y_condition:
+            if ty > by:
+                self.yspeed = self.move_speed_y
+            else:
+                self.yspeed = self.move_speed_y * -1
