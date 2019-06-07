6040e466f4a1a403978b52d14cf4046189ca1ac4
KarmenovA <karmenov_am@network.bkv-pvl.kz>
2019-06-07 09:57:07 +0600

    human attack detection improved, attack ending bound on animation end, functions moved in alphabet order

--- Units/Human/Human.py
+++ Units/Human/Human.py
@@ -11,12 +11,11 @@ class Human:
         self.image = image.load(resource_path("Media/Sprites/Units/Human/human.png"))
         self.rect = self.image.get_rect(center=pos)
 
-        self.animations = HumanAnimations(100)
+        # region SELF VARIABLES ----------------------------------------------------------
+        self.animations = HumanAnimations(200)
         # region ATTACK
-        self.attack_cd = 1500
-        self.attack_cur_cd = 1500
         self.attack_damage = 10
-        self.attack_range = 40
+        self.attack_range = 45
         self.attack_target = None
         # endregion
         self.body_height = self.rect.width * 0.23
@@ -33,58 +32,78 @@ class Human:
         self.move_speed_x = 2.0
         self.move_speed_y = 1.0
         self.selected = False
-        self.shadow_rect = self.half_rect.copy()
         self.state = state
         self.target = self.rect.center
         self.team = team
-        self.xvel = 0
-        self.xspeed = 0
-        self.yvel = 0
-        self.yspeed = 0
-        self.z = 0
-        # !!!!!!!!!!!!!!!!--------------------------------
-        self.font = font.SysFont('Arial', 14)
+        # region VELOCITY, SPEED AND Z
+        self.xvel = 0.0
+        self.xspeed = 0.0
+        self.yvel = 0.0
+        self.yspeed = 0.0
+        self.z = 0.0
+        # endregion
+        # endregion
 
-    @staticmethod
-    def get_distance(p1, p2):
-        return math.hypot(p1[0] - p2[0], p1[1] - p2[1])
+    def able_to_attack(self):
+        if self.attack_target is None:
+            return False
+        return self.attack_target.hp > 0 and self.get_dist_to_attack_trgt() < self.attack_range\
+            and self.attack_target.state != "dead" and self.in_target_y_width()
 
-    def get_dist_to_attack_trgt(self):
-        if self.attack_target is not None:
-            return self.get_distance(self.rect.midbottom, self.attack_target.rect.midbottom)
+    def attack_start(self):
+        self.animations.Attack_left.play()
+        self.animations.Attack_right.play()
+        self.set_dir_to(self.attack_target)
+        self.state = "attack"
+
+    def attack_stop(self):
+        self.animations.Attack_left.stop()
+        self.animations.Attack_right.stop()
+        self.state = "stand"
+
+    def close_to_attack_target(self):
+        if self.end_fight():
+            self.stop_attacking()
+            return
+        x1, y1 = self.rect.midbottom
+        x2, y2 = self.attack_target.rect.midbottom
+        tx, ty = self.attack_target.rect.midbottom
+        distance_to_move = self.attack_range / 2.5
+        if not self.end_attacking() and self.able_to_attack():
+            self.attack_start()
+            return
+        if x1 > x2:
+            tx += distance_to_move
         else:
-            return 0
+            tx -= distance_to_move
+        self.target = (tx, ty)
 
-    def end_of_attacking(self):
-        if self.attack_target is None:
-            return True
-        return ((self.attack_target.hp < 1) or (self.get_dist_to_attack_trgt() > self.attack_range)
-                or (self.state != "attack") or (self.attack_target.state == "dead"))
-
-    def end_of_fight(self):
-        if self.attack_target is None:
-            return True
-        return ((self.attack_target.hp < 1) or (self.get_dist_to_attack_trgt() > self.enemy_detect_range)
-                or (self.state != "attack") or (self.attack_target.state == "dead"))
+    def die(self):
+        self.yvel = -4.0
+        if self.killer.rect.center[0] > self.rect.center[0]:
+            self.xvel = -6.0
+        else:
+            self.xvel = 6.0
+        self.state = "dead"
+        self.target = self.rect.midbottom
+        self.attack_target = None
+        if self.dir == 0:
+            self.image = image.load(resource_path("Media/Sprites/Units/Human/human_dead_left.png"))
+        else:
+            self.image = image.load(resource_path("Media/Sprites/Units/Human/human_dead_right.png"))
 
     def draw(self, screen):
         def draw_hp_bar():
             draw.rect(screen, options.team_colors[self.team], Rect(self.rect.x + 2, self.rect.y - 12, (self.max_hp / 4) + 2, 3), 1)  # контур полоски hp
-            draw.rect(screen, options.team_colors[self.team], Rect(self.rect.x + 3, self.rect.y - 11, self.hp / 4, 1), 0)             # текущее количество hp
-
-        def stop_moving():
-            self.xspeed = 0
-            self.yspeed = 0
-            self.z = 0
-            self.target = self.rect.midbottom
+            draw.rect(screen, (70, 200, 70), Rect(self.rect.x + 3, self.rect.y - 11, self.hp / 4, 1), 0)             # текущее количество hp
 
         def walk_to_target():
             bx, by = self.rect.midbottom
             tx, ty = self.target
-            x_condition = bx - (self.half_rect.width*0.7) <= self.target[0] <= bx + (self.half_rect.width*0.7)
-            y_condition = by - (self.body_height*0.7) <= self.target[1] <= by + (self.body_height*0.7)
+            x_condition = bx - (self.half_rect.width*0.6) <= tx <= bx + (self.half_rect.width*0.6)
+            y_condition = by - (self.body_height*0.6) <= ty <= by + (self.body_height*0.6)
             if x_condition and y_condition:
-                stop_moving()
+                self.stop_moving()
                 self.state = "stand"
                 return
             if x_condition:
@@ -104,58 +123,32 @@ class Human:
                 else:
                     self.yspeed = self.move_speed_y * -1
 
-        def close_to_attack_target():
-            x1, y1 = self.rect.midbottom
-            x2, y2 = self.attack_target.rect.midbottom
-            tx, ty = self.attack_target.rect.midbottom
-            distance_to_move = 0
-            if self.get_dist_to_attack_trgt() > self.attack_range:
-                distance_to_move = self.get_dist_to_attack_trgt() - self.attack_range
-
-            if x1 > x2:
-                tx += distance_to_move
-            else:
-                tx -= distance_to_move
-            self.target = (tx, ty)
-
-        def die():
-            self.yvel = -40
-            if self.killer.rect.center[0] > self.rect.center[0]:
-                self.xvel = -10
-            else:
-                self.xvel = 10
-            self.state = "dead"
-            self.target = self.rect.midbottom
-            self.attack_target = None
-            if self.dir == 0:
-                self.image = image.load(resource_path("Media/Sprites/Units/Human/human_dead_left.png"))
-            else:
-                self.image = image.load(resource_path("Media/Sprites/Units/Human/human_dead_right.png"))
-
         def land_hit():
-            self.attack_cur_cd = self.attack_cd
+            # draw.circle(screen, (0, 0, 255), self.rect.center, 15, 0)
+            self.set_dir_to(self.attack_target)
+            self.attack_start()
             if self.get_dist_to_attack_trgt() <= self.attack_range and self.attack_target is not None:
                 self.attack_target.hp -= self.attack_damage
                 if self.attack_target.hp < 1:
                     self.attack_target.killer = self
-            if self.end_of_attacking():
-                if self.end_of_fight():
+            if self.end_attacking() or not self.able_to_attack():
+                if self.end_fight():
                     self.attack_target = None
-                self.state = "stand"
+                self.attack_stop()
 
         self.half_rect.center = self.rect.midbottom
-        self.shadow_rect.x, self.shadow_rect.y = self.half_rect.x, (self.half_rect.y + self.z)
-
         if self.hp > 0:
             draw_hp_bar()
             if not self.half_rect.collidepoint(self.target) and (self.state != "drag") and (self.state != "falling"):
                 self.state = "moving"
             if self.selected:
-                draw.ellipse(screen, options.team_colors[self.team], self.half_rect, 1)
-                draw.circle(screen, options.team_colors[self.team], self.rect.midbottom, self.attack_range, 2)
+                draw.ellipse(screen, options.team_colors[self.team], self.half_rect, 4)
+                # draw.circle(screen, options.team_colors[self.team], self.rect.midbottom, self.attack_range, 2)
                 draw.circle(screen, options.team_colors[self.team], self.rect.midbottom, self.enemy_detect_range, 1)
+            else:
+                draw.ellipse(screen, options.team_colors[self.team], self.half_rect, 1)
         elif self.state != "dead":
-            die()
+            self.die()
 
         if self.state == "drag":
             self.animations.Drag.blit(screen, self.rect.midtop)
@@ -164,53 +157,114 @@ class Human:
             draw.line(screen, options.team_colors[self.team], self.target, self.rect.center, 1)
             walk_to_target()
             if self.attack_target is not None:
-                if self.get_dist_to_attack_trgt() <= self.attack_range:
-                    self.state = "attack"
-                else:
-                    close_to_attack_target()
+                self.close_to_attack_target()
             if self.dir == 0:
                 self.animations.Walk_left.blit(screen, (self.rect.x, self.rect.y))
             else:
                 self.animations.Walk_right.blit(screen, (self.rect.x, self.rect.y))
 
         elif self.state == "stand":
-            stop_moving()
+            self.stop_moving()
             if self.attack_target is not None:
-                if self.get_dist_to_attack_trgt() <= self.attack_range:
-                    self.state = "attack"
-                else:
-                    close_to_attack_target()
+                    self.close_to_attack_target()
             if self.dir == 0:
                 self.animations.Stand_left.blit(screen, (self.rect.x, self.rect.y))
             else:
                 self.animations.Stand_right.blit(screen, (self.rect.x, self.rect.y))
 
         elif self.state == "falling":
-            self.z -= self.yspeed
             if self.dir == 0:
                 self.image = image.load(resource_path("Media/Sprites/Units/Human/human_falling_left.png"))
             else:
                 self.image = image.load(resource_path("Media/Sprites/Units/Human/human_falling_right.png"))
             screen.blit(self.image, self.rect)
-            if self.z < 0:
-                self.z = 0
+            if self.z <= 0.0:
                 self.state = "stand"
 
         elif self.state == "dead":
             screen.blit(self.image, self.rect)
 
         elif self.state == "attack":
-            if self.attack_cur_cd <= 0:
-                land_hit()
-            else:
-                self.attack_cur_cd -= 100
-            if self.rect.midbottom[0] > self.target[0]:
+            if self.end_fight():
+                self.attack_stop()
+            if self.dir == 0:
+                if self.animations.Attack_left.isFinished():
+                    land_hit()
+                    return
                 self.animations.Attack_left.blit(screen, (self.rect.x, self.rect.y))
             else:
+                if self.animations.Attack_right.isFinished():
+                    land_hit()
+                    return
                 self.animations.Attack_right.blit(screen, (self.rect.x, self.rect.y))
 
         elif self.state == "attack_move":
             pass
 
+    def end_attacking(self):
+        if self.attack_target is None:
+            return True
+        return ((self.attack_target.hp < 1) or (self.get_dist_to_attack_trgt() > self.attack_range)
+                or (self.attack_target.state == "dead"))
+
+    def end_fight(self):
+        if self.attack_target is None:
+            return True
+        return (self.attack_target.hp < 1 or self.attack_target.state == "dead"
+                or self.get_dist_to_attack_trgt() > self.enemy_detect_range)
+
+    @staticmethod
+    def get_distance(p1, p2):
+        return math.hypot(p1[0] - p2[0], p1[1] - p2[1])
+
+    def get_dist_to_attack_trgt(self):
+        if self.attack_target is not None:
+            return self.get_distance(self.rect.midbottom, self.attack_target.rect.midbottom)
+        else:
+            return 0
+
+    def in_target_y_width(self):
+        if self.attack_target is None:
+            return False
+        selfy = self.rect.midbottom[1]
+        trgty = self.attack_target.rect.midbottom[1]
+        trgth = self.attack_target.body_height
+        return trgty + trgth > selfy > trgty - trgth
+
     def move_ip(self, pos):
         self.rect.topright = pos
+
+    def set_dir_to(self, something):
+        slfx, slfy = self.rect.midbottom
+        trgtx, trgty = 0, 0
+        if hasattr(something, 'rect') and isinstance(something.rect, Rect):
+            trgtx = something.rect.midbottom[0]
+        elif isinstance(something, list):
+            trgtx = something[0]
+        elif isinstance(something, int):
+            trgtx = something
+        if slfx > trgtx:
+            self.dir = 0
+        else:
+            self.dir = 1
+
+    def stop_attacking(self):
+        self.stop_moving()
+        self.attack_target = None
+        self.state = "stand"
+
+    def stop_moving(self):
+        self.xspeed = 0
+        self.yspeed = 0
+        self.z = 0.0
+
+        self.target = self.rect.midbottom
+
+# if self.attack_stand_time > 0:
+#     self.attack_stand_time - options.milliseconds
+#     if self.dir == 0:
+#         self.image = image.load(resource_path("Media/Sprites/Units/Human/Attack/Left/human_attack_left3.png"))
+#     else:
+#         self.image = image.load(resource_path("Media/Sprites/Units/Human/Attack/Right/human_attack_right3.png"))
+#     screen.blit(self.image, self.rect)
+#     return
--- Units/Human/human_animations.py
+++ Units/Human/human_animations.py
@@ -5,59 +5,70 @@ import pyganim
 class HumanAnimations:
     def __init__(self, speed):
         self.speed = speed / 100  # проценты перевожу в десятые доли
-        self.Stand_right_pic = [(resource_path("Media/Sprites/Units/Human/Stand/human_stand_right1.png"), 200/self.speed),
-                                (resource_path("Media/Sprites/Units/Human/Stand/human_stand_right2.png"), 300/self.speed),
-                                (resource_path("Media/Sprites/Units/Human/Stand/human_stand_right3.png"), 700/self.speed),
-                                (resource_path("Media/Sprites/Units/Human/Stand/human_stand_right2.png"), 300/self.speed),
-                                (resource_path("Media/Sprites/Units/Human/Stand/human_stand_right1.png"), 200/self.speed)]
+        self.Stand_right_pic = [(resource_path("Media/Sprites/Units/Human/Stand/human_stand_right1.png"), 200),
+                                (resource_path("Media/Sprites/Units/Human/Stand/human_stand_right2.png"), 300),
+                                (resource_path("Media/Sprites/Units/Human/Stand/human_stand_right3.png"), 700),
+                                (resource_path("Media/Sprites/Units/Human/Stand/human_stand_right2.png"), 300),
+                                (resource_path("Media/Sprites/Units/Human/Stand/human_stand_right1.png"), 200)]
 
-        self.Stand_left_pic = [(resource_path("Media/Sprites/Units/Human/Stand/human_stand_left3.png"), 800/self.speed),
-                               (resource_path("Media/Sprites/Units/Human/Stand/human_stand_left1.png"), 300/self.speed),
-                               (resource_path("Media/Sprites/Units/Human/Stand/human_stand_left2.png"), 400/self.speed),
-                               (resource_path("Media/Sprites/Units/Human/Stand/human_stand_left1.png"), 300/self.speed),
-                               (resource_path("Media/Sprites/Units/Human/Stand/human_stand_left3.png"), 200/self.speed)]
+        self.Stand_left_pic = [(resource_path("Media/Sprites/Units/Human/Stand/human_stand_left3.png"), 800),
+                               (resource_path("Media/Sprites/Units/Human/Stand/human_stand_left1.png"), 300),
+                               (resource_path("Media/Sprites/Units/Human/Stand/human_stand_left2.png"), 400),
+                               (resource_path("Media/Sprites/Units/Human/Stand/human_stand_left1.png"), 300),
+                               (resource_path("Media/Sprites/Units/Human/Stand/human_stand_left3.png"), 200)]
 
-        self.Drag_pic = [(resource_path("Media/Sprites/Units/Human/Drag/human_Drag21.png"), 200/self.speed),
-                         (resource_path("Media/Sprites/Units/Human/Drag/human_Drag22.png"), 300/self.speed),
-                         (resource_path("Media/Sprites/Units/Human/Drag/human_Drag23.png"), 300/self.speed),
-                         (resource_path("Media/Sprites/Units/Human/Drag/human_Drag22.png"), 300/self.speed),
-                         (resource_path("Media/Sprites/Units/Human/Drag/human_Drag21.png"), 200/self.speed)]
+        self.Drag_pic = [(resource_path("Media/Sprites/Units/Human/Drag/human_Drag21.png"), 200),
+                         (resource_path("Media/Sprites/Units/Human/Drag/human_Drag22.png"), 300),
+                         (resource_path("Media/Sprites/Units/Human/Drag/human_Drag23.png"), 300),
+                         (resource_path("Media/Sprites/Units/Human/Drag/human_Drag22.png"), 300),
+                         (resource_path("Media/Sprites/Units/Human/Drag/human_Drag21.png"), 200)]
 
-        self.Walk_left_pic = [(resource_path("Media/Sprites/Units/Human/Walk/Left/human_walk_left1.png"), 200/self.speed),
-                              (resource_path("Media/Sprites/Units/Human/Walk/Left/human_walk_left2.png"), 200/self.speed),
-                              (resource_path("Media/Sprites/Units/Human/Walk/Left/human_walk_left3.png"), 200/self.speed),
-                              (resource_path("Media/Sprites/Units/Human/Walk/Left/human_walk_left4.png"), 200/self.speed),
-                              (resource_path("Media/Sprites/Units/Human/Walk/Left/human_walk_left5.png"), 200/self.speed)]
+        self.Walk_left_pic = [(resource_path("Media/Sprites/Units/Human/Walk/Left/human_walk_left1.png"), 200),
+                              (resource_path("Media/Sprites/Units/Human/Walk/Left/human_walk_left2.png"), 200),
+                              (resource_path("Media/Sprites/Units/Human/Walk/Left/human_walk_left3.png"), 200),
+                              (resource_path("Media/Sprites/Units/Human/Walk/Left/human_walk_left4.png"), 200),
+                              (resource_path("Media/Sprites/Units/Human/Walk/Left/human_walk_left5.png"), 200)]
 
-        self.Walk_right_pic = [(resource_path("Media/Sprites/Units/Human/Walk/Right/human_walk_right1.png"), 200/self.speed),
-                               (resource_path("Media/Sprites/Units/Human/Walk/Right/human_walk_right2.png"), 200/self.speed),
-                               (resource_path("Media/Sprites/Units/Human/Walk/Right/human_walk_right3.png"), 200/self.speed),
-                               (resource_path("Media/Sprites/Units/Human/Walk/Right/human_walk_right4.png"), 200/self.speed),
-                               (resource_path("Media/Sprites/Units/Human/Walk/Right/human_walk_right5.png"), 200/self.speed)]
+        self.Walk_right_pic = [(resource_path("Media/Sprites/Units/Human/Walk/Right/human_walk_right1.png"), 200),
+                               (resource_path("Media/Sprites/Units/Human/Walk/Right/human_walk_right2.png"), 200),
+                               (resource_path("Media/Sprites/Units/Human/Walk/Right/human_walk_right3.png"), 200),
+                               (resource_path("Media/Sprites/Units/Human/Walk/Right/human_walk_right4.png"), 200),
+                               (resource_path("Media/Sprites/Units/Human/Walk/Right/human_walk_right5.png"), 200)]
 
-        self.Attack_left_pic = [(resource_path("Media/Sprites/Units/Human/Attack/Left/human_attack_left1.png"), 300/self.speed),
-                                (resource_path("Media/Sprites/Units/Human/Attack/Left/human_attack_left2.png"), 300/self.speed),
-                                (resource_path("Media/Sprites/Units/Human/Attack/Left/human_attack_left3.png"), 300/self.speed),
-                                (resource_path("Media/Sprites/Units/Human/Attack/Left/human_attack_left4.png"), 300/self.speed),
-                                (resource_path("Media/Sprites/Units/Human/Attack/Left/human_attack_left2.png"), 300/self.speed)]
+        self.Attack_left_pic = [(resource_path("Media/Sprites/Units/Human/Attack/Left/human_attack_left1.png"), 600),
+                                (resource_path("Media/Sprites/Units/Human/Attack/Left/human_attack_left2.png"), 400),
+                                (resource_path("Media/Sprites/Units/Human/Attack/Left/human_attack_left4.png"), 200),
+                                (resource_path("Media/Sprites/Units/Human/Attack/Left/human_attack_left3.png"), 300)]
 
-        self.Attack_right_pic = [(resource_path("Media/Sprites/Units/Human/Attack/Right/human_attack_right1.png"), 300/self.speed),
-                                 (resource_path("Media/Sprites/Units/Human/Attack/Right/human_attack_right2.png"), 300/self.speed),
-                                 (resource_path("Media/Sprites/Units/Human/Attack/Right/human_attack_right3.png"), 300/self.speed),
-                                 (resource_path("Media/Sprites/Units/Human/Attack/Right/human_attack_right4.png"), 300/self.speed),
-                                 (resource_path("Media/Sprites/Units/Human/Attack/Right/human_attack_right2.png"), 300/self.speed)]
+        self.Attack_right_pic = [(resource_path("Media/Sprites/Units/Human/Attack/Right/human_attack_right1.png"), 600),
+                                 (resource_path("Media/Sprites/Units/Human/Attack/Right/human_attack_right2.png"), 400),
+                                 (resource_path("Media/Sprites/Units/Human/Attack/Right/human_attack_right4.png"), 200),
+                                 (resource_path("Media/Sprites/Units/Human/Attack/Right/human_attack_right3.png"), 300)]
 
-        self.Attack_left = pyganim.PygAnimation(self.Attack_left_pic)
-        self.Attack_left.play()
-        self.Attack_right = pyganim.PygAnimation(self.Attack_right_pic)
-        self.Attack_right.play()
-        self.Stand_right = pyganim.PygAnimation(self.Stand_right_pic)
-        self.Stand_right.play()
-        self.Stand_left = pyganim.PygAnimation(self.Stand_left_pic)
-        self.Stand_left.play()
+        self.Attack_left = pyganim.PygAnimation(self.Attack_left_pic, loop=False)
+        self.Attack_left._rate = self.speed
+        self.Attack_right = pyganim.PygAnimation(self.Attack_right_pic, loop=False)
+        self.Attack_right._rate = self.speed
         self.Drag = pyganim.PygAnimation(self.Drag_pic)
         self.Drag.play()
+        self.Stand_right = pyganim.PygAnimation(self.Stand_right_pic)
+        self.Stand_right._rate = self.speed
+        self.Stand_right.play()
+        self.Stand_left = pyganim.PygAnimation(self.Stand_left_pic)
+        self.Stand_left._rate = self.speed
+        self.Stand_left.play()
         self.Walk_left = pyganim.PygAnimation(self.Walk_left_pic)
+        self.Walk_left._rate = self.speed
         self.Walk_left.play()
         self.Walk_right = pyganim.PygAnimation(self.Walk_right_pic)
         self.Walk_right.play()
+        self.Walk_right._rate = self.speed
+
+    def get_anim_speed(self, anim):
+        anim_array = self.Attack_left_pic
+        ms_sum = 0
+        if anim == "Attack":
+            anim_array = self.Attack_left_pic
+        for frame in anim_array:
+            ms_sum += frame[1]
+        return ms_sum
